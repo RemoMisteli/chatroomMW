3ddfa24b6d3f2ca4b9dcbed5df94d2952892dbc2
Misteli Remo <rmisteli@fluance.ch>
2020-09-25 17:30:28 +0200

    change response of first endpoint

--- src/main/java/ch/apprentice/chatroom/controller/MessageController.java
+++ src/main/java/ch/apprentice/chatroom/controller/MessageController.java
@@ -1,6 +1,11 @@
 package ch.apprentice.chatroom.controller;
 
+
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("chatroom/messages")
 public class MessageController {
 	
-	
+	@CrossOrigin(origins = "*")
 	@GetMapping(path = "")
 	public String getMessage() {
-		return "test";
+		Date date= new Date();
+		
+		return date.toString();
 	}
 
+	
+	@CrossOrigin(origins = "*")
+	@GetMapping(path = "/send/{name}/{message}")
+	public void setMessage(@PathVariable String name, @PathVariable String message) {
+
+		System.out.println();
+		System.out.println();
+
+		System.out.println(name + " " + message);
+
+
+	}
 }
