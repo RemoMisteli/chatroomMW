38187231b031e65adf999334084a15568a214353
Misteli Remo <rmisteli@fluance.ch>
2020-09-25 11:43:15 +0200

    add CrossOrigin

--- src/main/java/ch/apprentice/chatroom/controller/MessageController.java
+++ src/main/java/ch/apprentice/chatroom/controller/MessageController.java
@@ -1,5 +1,6 @@
 package ch.apprentice.chatroom.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("chatroom/messages")
 public class MessageController {
 	
-	
+	@CrossOrigin(origins = "*")
 	@GetMapping(path = "")
 	public String getMessage() {
 		return "test";
